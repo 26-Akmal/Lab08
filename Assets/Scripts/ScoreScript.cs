8ad4ef088cf7916369934ad0c2d4c3e23cb1b629
SAMDOL <akmal5000sayhello@gmail.com>
2022-10-26 11:30:42 +0800

    Unity Final Work

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private float score;
+    public Text ScoreUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreUI.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreUI.text = "Score: " + score;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            score += 10;
+        }
+    }
+}
